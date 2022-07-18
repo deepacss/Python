dd2f95b5dcb8d5198f30cc10e1bdbb55619bdf7e
deepacss <deepasundaram96@gmail.com>
2022-07-18 18:21:08 +0530

    PrimeNumber btn 1 to n

---
+++ Basics/CheckPrime1toN.py
@@ -0,0 +1,13 @@
+
+#check the prime number between 1 to n
+lower=int(input("Enter the lower interval:"))
+upper=int(input("Enter the upper interval:"))
+
+
+for num in range(lower,upper+1):
+    if(num>1):
+        for i in range(2,num):
+            if(num%i==0):
+                break
+        else:
+            print(num)
